78fea5f9534913a8038c9a5ed2f098955620c4aa
Muhammad A.G. Bello <magbello157@gmail.com>
2024-04-30 00:27:27 +0100

    style the navbar active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
 
 function NavBar() {
+  const currentPath = usePathname();
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
@@ -17,7 +23,11 @@ function NavBar() {
         {links.map(({ label, href }) => (
           <Link
             key={href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              'text-zinc-900': href === currentPath,
+              'text-zinc-500': href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
             href={href}
           >
             {label}
